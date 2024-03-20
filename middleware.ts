237d9a2163da470a9a5cc0f255e64b234e456b7f
Halazv2 <halaziani2003@gmail.com>
2024-03-20 19:53:01 +0000

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks/clerk(.*)"],
+  publicRoutes: ["/api/webhooks(.*)"],
 });
 
 export const config = {
