da7f21213c135640c84144e6b3de45f1733cee0d
Halazv2 <halaziani2003@gmail.com>
2024-03-24 16:04:48 +0000

    Update import statements in image.model.ts

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,4 +1,4 @@
-import { Document, Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from 'mongoose';
 
 export interface IImage extends Document {
   title: string;
@@ -33,11 +33,11 @@ const ImageSchema = new Schema({
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
-const Image = models?.Image || model("Image", ImageSchema);
+const Image = models?.Image || model('Image', ImageSchema);
 
 export default Image;
