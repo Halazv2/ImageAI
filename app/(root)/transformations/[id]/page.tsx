7ea3300c83ddad2ee838115104c60443ccb0fc5a
Halazv2 <halaziani2003@gmail.com>
2024-03-25 20:36:30 +0000

    Refactor transformations page to display image details

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -1,7 +1,98 @@
-import React from "react";
+import { auth } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import Header from '@/components/Shared/Header';
+import TransformedImage from '@/components/Shared/TransformedImage';
+import { Button } from '@/components/ui/button';
+import { getImageById } from '@/lib/actions/image.actions';
+import { getImageSize } from '@/lib/utils';
+import { transformationTypes } from '@/constants';
 
-const TransformationsPage = () => {
-  return <div>TransformationsPage</div>;
+const ImageDetails = async ({ params: { id } }: SearchParamProps) => {
+  const { userId } = auth();
+
+  const image = await getImageById(id);
+
+  return (
+    <>
+      <Header title={image.title} />
+
+      <section className="mt-5 flex flex-wrap gap-4">
+        <div className="p-14-medium md:p-16-medium flex gap-2">
+          <p className="text-dark-600">Transformation:</p>
+          <p className="capitalize text-purple-400">
+            {
+              transformationTypes[
+                image.transformationType as TransformationTypeKey
+              ].title
+            }
+          </p>
+        </div>
+
+        {image.prompt && (
+          <>
+            <div className="p-14-medium md:p-16-medium flex gap-2 ">
+              <p className="text-dark-600">Prompt:</p>
+              <p className=" capitalize text-purple-400">{image.prompt}</p>
+            </div>
+          </>
+        )}
+
+        {image.color && (
+          <>
+            <div className="p-14-medium md:p-16-medium flex gap-2">
+              <p className="text-dark-600">Color:</p>
+              <p className=" capitalize text-purple-400">{image.color}</p>
+            </div>
+          </>
+        )}
+
+        {image.aspectRatio && (
+          <>
+            <div className="p-14-medium md:p-16-medium flex gap-2">
+              <p className="text-dark-600">Aspect Ratio:</p>
+              <p className=" capitalize text-purple-400">{image.aspectRatio}</p>
+            </div>
+          </>
+        )}
+      </section>
+
+      <section className="mt-10 border-t border-dark-400/15">
+        <div className="transformation-grid">
+          <div className="flex flex-col gap-4">
+            <h3 className="h3-bold text-dark-600">Original</h3>
+
+            <Image
+              width={getImageSize(image.transformationType, image, 'width')}
+              height={getImageSize(image.transformationType, image, 'height')}
+              src={image.secureURL}
+              alt="image"
+              className="transformation-original_image"
+            />
+          </div>
+
+          <TransformedImage
+            image={image}
+            type={image?.transformationType}
+            title={image?.title}
+            isTransforming={false}
+            transformationConfig={image?.config}
+            hasDownload={true}
+          />
+        </div>
+
+        {userId === image?.author?.clerkId && (
+          <div className="mt-4 space-y-4">
+            <Button asChild type="button" className="submit-button capitalize">
+              <Link href={`/transformations/${image._id}/update`}>
+                Update Image
+              </Link>
+            </Button>
+          </div>
+        )}
+      </section>
+    </>
+  );
 };
 
-export default TransformationsPage;
+export default ImageDetails;
