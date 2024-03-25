0dc430832e585d8445667bf32b74820ef478a5c0
Halazv2 <halaziani2003@gmail.com>
2024-03-25 18:09:14 +0000

    Update Home component and Add Collection component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,52 @@
-import React from "react";
+import { Collection } from '@/components/Shared/Collection';
+import { navLinks } from '@/constants';
+import { getAllImages } from '@/lib/actions/image.actions';
 
-const Home = () => {
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({ page, searchQuery });
   return (
-    <div>
-      <h1>ImageAi</h1>
-    </div>
+    <>
+      <section className="home">
+        <h1 className="home-heading">
+          Unleash Your Creative Vision with Image AI
+        </h1>
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex-center flex-col gap-2"
+            >
+              <li className="flex-center w-fit rounded-full bg-white p-4">
+                <Image
+                  src={link.icon}
+                  alt={link.label}
+                  width={24}
+                  height={24}
+                />
+              </li>
+              <p className="p-14-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
+      </section>
+    </>
   );
 };
 
